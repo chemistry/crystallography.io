95b839c32e7070a145141c6bdb6e96e8853dde5f
Volodymyr Vreshch <vreshch@gmail.com>
2020-01-20 02:07:18 +0200

    Structures List Endpoint

--- packages/functions/api-get-structures-list/function.check.ts
+++ packages/functions/api-get-structures-list/function.check.ts
@@ -1,12 +1,33 @@
+import { handler } from './index';
+class mockRes {
+    public status(stat: number) {
+      console.log(stat);
+      return this;
+    }
+    public json(content: any) {
+      console.log(content);
+      return this;
+    }
+    public send(content: string) {
+      console.log(content);
+      return this;
+    }
+}
+
 // tslint:disable-next-line
-console.time("check");
+
 (async () => {
     // Check event function
+    const res: any = new mockRes();
 
-})().then(() => {
-    // tslint:disable-next-line
+    await new Promise((res)=> {
+      setTimeout(res, 3000)
+    });
+
+    console.time("check");
+    await handler({ query: { page: 1} } as any, res);
     console.timeEnd("check");
-}).catch((e) => {
+})().catch((e) => {
     // tslint:disable-next-line
     console.log(e);
 });
--- packages/functions/api-get-structures-list/index.ts
+++ packages/functions/api-get-structures-list/index.ts
@@ -1,8 +1,47 @@
+import { Firestore } from "@google-cloud/firestore";
+import * as Joi from "@hapi/joi";
 import { Request, Response } from "express";
+import { mapStructure } from "./helpers";
+
+const structurePageValidation = Joi.number().integer().min(1).max(99999);
+const firestore = new Firestore();
+
+const PER_PAGE = 100;
+const structureMaper = mapStructure();
 
 export async function handler(
   req: Request, res: Response,
 ) {
-    const message = req.query.message || req.body.message || "Hello World!";
-    res.status(200).send(message);
+    const page = req.query.page || 1;
+    const validationRes = structurePageValidation.validate(page);
+    if (validationRes.error) {
+        return res.status(400).json({
+            errors: [{
+              code: 400,
+              message: "Incorrect page",
+              details: "Incorrect page",
+            }],
+        });
+    }
+
+    return firestore
+      .collection("structures")
+      .limit(PER_PAGE)
+      .offset((page - 1) * PER_PAGE)
+      .get()
+      .then((querySnapshot) => {
+          const data = querySnapshot.docs.map((doc) => {
+              return {
+                id: doc.id,
+                ...doc.data(),
+              };
+          })
+          .map(structureMaper);
+
+          return res.status(200).json({
+            errors: [],
+            meta: {},
+            data,
+          });
+      });
 }
