dc2dfecd6cd5b029c2d5efc7b8e0269c87f8e142
Volodymyr Vreshch <vreshch@gmail.com>
2020-01-22 01:40:06 +0200

    Correct build

--- packages/containers/application-web-manager/deploy.sh
+++ packages/containers/application-web-manager/deploy.sh
@@ -2,4 +2,11 @@
 
 docker build -t gcr.io/crystallography-api/application-web-manager -f Dockerfile ../../../
 docker push gcr.io/crystallography-api/application-web-manager
-gcloud beta run deploy application-web-manager --image gcr.io/crystallography-api/application-web-manager:latest --region europe-west1 --concurrency 20 --timeout 10s --platform managed --quiet
+gcloud run deploy application-web-manager \
+    --project crystallography-api \
+    --image gcr.io/crystallography-api/application-web-manager \
+    --allow-unauthenticated \
+    --region europe-west1 \
+    --concurrency 20 \
+    --timeout 10s \
+    --platform managed
