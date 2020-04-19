c74fe27bf3e18b2b89e28e835d371b59a69a1f74
Volodymyr Vreshch <vreshch@gmail.com>
2020-04-19 21:32:35 +0300

    Add Application Styling

--- packages/containers/crystallography-web/src/common/index.tsx
+++ packages/containers/crystallography-web/src/common/index.tsx
@@ -1,5 +1,10 @@
 import * as React from "react";
 
+if (process.env.BROWSER) {
+    // tslint:disable-next-line
+    require("./index.scss");
+}
+
 export enum AppContextType {
     frontend = "frontend",
     backend = "backend",
@@ -13,7 +18,22 @@ export interface Application {
 export type ApplicationFactory = (context: ApplicationContext) =>  Promise<Application>;
 
 export const getApplication: ApplicationFactory = async (context: ApplicationContext) => {
-  const App = () => (<h1>App</h1>);
+  const App = () => (<div className="app">
+    <div className="top-navigation container bg-primary">
+        <div className="columns">
+            <div className="column col-12 top-navigation__row">&nbsp;</div>
+        </div>
+    </div>
+    <div className="container main-content">
+        <div className="columns">
+          <div className="column col-3 bg-dark">
+          </div>
+          <div className="column col-9">
+              <h1>App</h1>
+          </div>
+        </div>
+    </div>
+  </div>);
 
   return Promise.resolve({ App });
 };
