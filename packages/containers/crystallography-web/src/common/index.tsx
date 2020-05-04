58971c9374616111328b51ef28c5db549b351c25
Volodymyr Vreshch <vreshch@gmail.com>
2020-05-05 02:14:42 +0300

    Add basic layout

--- packages/containers/crystallography-web/src/common/index.tsx
+++ packages/containers/crystallography-web/src/common/index.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+const { useState } = React;
 
 if (process.env.BROWSER) {
     // tslint:disable-next-line
@@ -18,6 +19,7 @@ export interface Application {
 export type ApplicationFactory = (context: ApplicationContext) =>  Promise<Application>;
 
 /*
+<h1>App</h1>
 <ul className="menu">
 <li className="divider" data-content="LINKS">
 </li>
@@ -53,23 +55,63 @@ export type ApplicationFactory = (context: ApplicationContext) =>  Promise<Appli
 </li>
 </ul>
 */
+/*
+  <div className="top-navigation container bg-primary">
+      <div className="columns">
+          <div className="column col-12 top-navigation__row">&nbsp;</div>
+      </div>
+  </div>
+  <div className="container main-content">
+      <div className="columns">
+        <div className="column col-3 bg-dark">
+        </div>
+        <div className="column col-9">
+
+        </div>
+      </div>
+  </div>
+*/
+
 export const getApplication: ApplicationFactory = async (context: ApplicationContext) => {
-  const App = () => (<div className="app">
-    <div className="top-navigation container bg-primary">
-        <div className="columns">
-            <div className="column col-12 top-navigation__row">&nbsp;</div>
+
+  const App = () => {
+      const [isOpen, setOpen] = useState(false);
+
+      return (<div className={`app ${isOpen ? "open" : ""}`}>
+        <div className="app-side-bar">
+            <div className="app-side-bar__menu">
+                <div className="app-menu-icon">
+                    <div className="app-menu-icon__wrap" onClick={() => setOpen(!isOpen)}>
+                        <i className="icon icon-menu"></i>
+                    </div>
+                </div>
+            </div>
+            <div className="app-menu-icon">
+                <div className="app-menu-icon__wrap">
+                    <i className="icon icon-search"></i>
+                </div>
+            </div>
+            <div className="app-menu-icon">
+                <div className="app-menu-icon__wrap">
+                    <i className="icon icon-people"></i>
+                </div>
+            </div>
+            <div className="app-menu-icon">
+                <div className="app-menu-icon__wrap">
+                  <i className="icon icon-shutdown"></i>
+                </div>
+            </div>
         </div>
-    </div>
-    <div className="container main-content">
-        <div className="columns">
-          <div className="column col-3 bg-dark">
-          </div>
-          <div className="column col-9">
-              <h1>App</h1>
-          </div>
+        <div className="app-layout">
+            <div className="app-header">
+                <div className="app-logo">crystallography.io</div>
+            </div>
+            <div className="app-content">
+            </div>
         </div>
-    </div>
-  </div>);
+      </div>);
+
+  };
 
   return Promise.resolve({ App });
 };
