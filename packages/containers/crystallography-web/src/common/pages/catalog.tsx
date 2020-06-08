0fe0105bbc02f5bba7451235da1170636e14a404
Volodymyr Vreshch <vreshch@gmail.com>
2020-06-08 04:04:30 +0300

    Application Loader

--- packages/containers/crystallography-web/src/common/pages/catalog.tsx
+++ packages/containers/crystallography-web/src/common/pages/catalog.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 import { RouteConfig } from "react-router-config";
 import { NavLink } from "react-router-dom";
 
+import { useLayoutEffect } from "react";
+import { useCallback } from "react";
+import { Loader } from "../components/loader";
 import { useLoadedData } from "../services";
 import { RootState } from "../store";
 
@@ -11,19 +14,24 @@ export const CatalogPage = (props: { route: RouteConfig }) => {
   const structures = useSelector((state: RootState) => state.catalogPage);
   useLoadedData(props.route);
 
+  const isLoading = useSelector((state: RootState) => state.catalogPage.isLoading);
+  const containerRef = useRef(null);
+
   return (
-      <div>
-          <header className="app-layout-header">
-              <h2 className="text-primary">Catalog</h2>
-          </header>
-          <div className="app-layout-content">
-            <ul>
-              <li><NavLink to="/catalog/1">1</NavLink></li>
-              <li><NavLink to="/catalog/2">2</NavLink></li>
-            </ul>
-            <hr />
-            <pre>{JSON.stringify(structures, null, 4)}</pre>
-          </div>
-      </div>
-  );
+        <div>
+            <header className="app-layout-header">
+                <h2 className="text-primary">Catalog</h2>
+            </header>
+            <div className="app-layout-content" id="content" ref={containerRef}>
+                <Loader isVisible={isLoading} scrollElement={containerRef}>
+                    <ul>
+                    <li><NavLink to="/catalog/1">1</NavLink></li>
+                    <li><NavLink to="/catalog/2">2</NavLink></li>
+                    </ul>
+                    <hr />
+                    <pre>{JSON.stringify(structures, null, 4)}</pre>
+                </Loader>
+            </div>
+        </div>
+    );
 };
