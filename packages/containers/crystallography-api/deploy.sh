9267aa54d05daee0448c9febebbfef1e81f0673c
Volodymyr Vreshch <vreshch@gmail.com>
2020-11-28 03:15:42 +0200

    Add enviroment varible

--- packages/containers/crystallography-api/deploy.sh
+++ packages/containers/crystallography-api/deploy.sh
@@ -6,6 +6,7 @@ gcloud run deploy crystallography-api \
     --project crystallography-api \
     --image gcr.io/crystallography-api/crystallography-api \
     --allow-unauthenticated \
+    --set-env-vars ES_KEY=$(echo $ES_KEY) \
     --region europe-west1 \
     --concurrency 20 \
     --timeout 10s \
