b588a4dfdfbf868117531cf2e703723884b9a30e
Volodymyr Vreshch <vreshch@gmail.com>
2020-12-09 02:12:09 +0200

    Syncronize COD with files

--- packages/swarm/cod-to-disk/src/app.ts
+++ packages/swarm/cod-to-disk/src/app.ts
@@ -1,13 +1,76 @@
+import * as path from 'path';
+import * as fs  from 'fs';
+import { ExecOptions, ShellString } from "shelljs";
+import { Readable, Transform, TransformCallback, Writable } from "stream";
+
 export interface AppContext {
   log: (message: string) => void;
-  exec: (command: string) => { code: number };
+  exec: (command: string, options?: ExecOptions & { async?: false }) => ShellString;
+  execAsync: (command: string) => Readable;
+  sendToQueue: (data: object) => void;
 }
 
-export async function App(context: AppContext) {
-    const { log, exec } = context;
+export interface CodFileRecord {
+    fileName: string;
+    codId: string;
+}
 
-    log("Synchronization cif form crystallography.net");
+const DATA_PATH = "/home/data/cif";
+const FILE_REGEX  = /^\w{1}\s+(([\w\d.\/]+\/(\d+)\.cif))$/i;
 
-    exec("echo 'hello world' ");
-    exec("sleep 50");
+let count = 0;
+const extractDataFromLogs = new Transform({
+    objectMode: true,
+    transform: (chunk: Buffer, encoding: BufferEncoding, callback: TransformCallback) => {
+        const lines = (chunk.toString('utf8'))
+            .split("\n")
+            .map((line) => {
+                const matches = FILE_REGEX.exec(line);
+                if (matches) {
+                    return  {
+                        fileName: path.resolve(DATA_PATH, matches[2]),
+                        codId: matches[3],
+                    };
+                }
+              })
+              .filter((item) => {
+                  return !!item;
+            });
+        count = count + lines.length;
+        callback(null, lines);
+    }
+});
+
+const getSendMessageToQueueStream = ({ sendToQueue }: AppContext)=> new Writable({
+    objectMode: true,
+    write: (chunk, _encoding, done) => {
+        sendToQueue(chunk)
+        done();
+    }
+});
+
+const fetchDataFromCod = ({ log, execAsync }: AppContext): Readable => {
+    const isFirstStart = !fs.existsSync(DATA_PATH);
+
+    if (isFirstStart) {
+        log('First start: initial fetching data...');
+        return execAsync("svn co svn://www.crystallography.net/cod/cif " + DATA_PATH);
+    }
+
+    log('Update SVN data...');
+    return execAsync("svn update " + DATA_PATH);
+}
+
+export const app = async(context: AppContext) => {
+    const { log } = context;
+
+    log('---------------------------------------------------');
+
+    fetchDataFromCod(context)
+       .pipe(extractDataFromLogs)
+       .pipe(getSendMessageToQueueStream(context))
+       .on('end', ()=> {
+            log(`Fetch data finished ${count} items were updated`);
+            log('---------------------------------------------------');
+        });
 }
--- packages/swarm/cod-to-disk/src/index.ts
+++ packages/swarm/cod-to-disk/src/index.ts
@@ -1,27 +1,54 @@
-// Imports the Google Cloud client library
+import { Readable } from "stream";
 import * as shell from "shelljs";
-import { App, AppContext } from "./app";
+import { ShellString, ExecOptions } from "shelljs";
+import { app, AppContext } from "./app";
 
-// tslint:disable-next-line
-console.time("Synchronization Action");
 
-const appContext: AppContext = {
-    log: (message: string) => {
+const QUEUE_NAME = 'COD_FILE_UPDATED';
+
+const getContext = async (): Promise<AppContext> => {
+
+    await new Promise(res => setTimeout(res, 15000));
+    const connection = await require('amqplib').connect('amqp://rabbitmq');
+    const chanel = await connection.createChannel();
+    await chanel.assertQueue(QUEUE_NAME);
+
+    process.on('exit', (code) => {
+         // tslint:disable-next-line
+        console.log(`About to exit with code: ${code}`);
+        chanel.close();
+    });
+
+    return {
+        log: (message: string) => {
+            // tslint:disable-next-line
+            console.log(message);
+        },
+        exec: (command: string, options?: ExecOptions & { async?: false }): ShellString => {
+            return shell.exec(command);
+        },
+        execAsync: (command: string): Readable => {
+            return shell.exec(command, { async:true }).stdout;
+        },
+        sendToQueue: (data: object): void => {
+            chanel.sendToQueue(QUEUE_NAME, Buffer.from(JSON.stringify(data)));
+        }
+    }
+}
+
+(async () => {
+    try {
+        const context = await getContext();
+
         // tslint:disable-next-line
-        console.log(message);
-    },
-    exec: (command: string) => {
-        return shell.exec(command);
-    },
-};
+        console.time('application start');
+        await app(context);
 
-App(appContext)
-    .then(() => {
         // tslint:disable-next-line
-        console.timeEnd("Synchronization Action");
-    })
-    .catch((e) => {
+        console.timeEnd('application start');
+    } catch(e) {
         // tslint:disable-next-line
         console.error(e);
         process.exit(-1);
-    });
+    }
+})();
