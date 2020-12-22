b37974683bd1fec336d40940bfdedc4267dec016
Volodymyr Vreshch <vreshch@gmail.com>
2020-12-23 00:03:06 +0200

    Add Structures List endpoint

---
+++ packages/swarm/cod-api/src/helpers/utils.ts
@@ -0,0 +1,107 @@
+const structureCommonAttributes = [
+    "a", "b", "c", "alpha", "beta", "gamma", "vol",
+    "commonname", "chemname", "mineral", "formula", "calcformula",
+    "__authors",
+];
+
+const structureCommonAttributesDetails = [
+    "diffrtemp", "diffrpressure",
+    "sg", "sgHall",
+    "radType", "wavelength",
+    "Rall", "Robs", "Rref", "wRall", "wRobs", "wRref",
+    "loops",
+];
+
+const structureCommonCalcAttributes = [
+    "title", "journal", "year", "volume", "issue", "firstpage", "lastpage", "doi",
+];
+
+export function getStructureAttributes(expand: boolean = false): string[] {
+    let res = [
+        ...structureCommonAttributes,
+        ...structureCommonCalcAttributes,
+    ];
+    if (expand) {
+        res = [
+            ...res,
+            ...structureCommonAttributesDetails,
+        ];
+    }
+
+    return res;
+}
+
+export function mapStructure(expand: boolean = false) {
+    const attributes = getStructureAttributes(expand);
+
+    return (item: any) => {
+        const itemFilted = attributes.reduce((acc: any, attr: string) => {
+            if (item.hasOwnProperty(attr)) {
+                acc[attr] = item[attr];
+            }
+            return acc;
+        }, {} as any);
+
+        if (Object.keys(item).length === 1) {
+            return  {
+                id: item._id,
+                type: "structure",
+                attributes: null,
+            };
+        }
+
+        return {
+            id: item._id,
+            type: "structure",
+            attributes: {
+                ...itemFilted,
+                id: item._id,
+                articleHtml: getarticleHtml(item),
+                loops: expand ? filterLoopArray(item.loops) : [],
+            },
+        };
+    };
+}
+
+function filterLoopArray(loops: any) {
+    if (!Array.isArray(loops)) {
+        return [];
+    }
+    return loops.filter((item) => {
+        return (item.columns.indexOf("_atom_site_fract_x") !== -1);
+    });
+}
+
+function getarticleHtml(item: any): string {
+    if (item.journal || item.year) {
+        const nameTags = [];
+
+        if (item.journal) {
+            nameTags.push("<i>" + item.journal + "</i>");
+        }
+
+        if (item.year) {
+            nameTags.push("(<b>" + item.year + "</b>)");
+        }
+
+        if (item.volume) {
+            nameTags.push(item.volume + ",");
+        }
+
+        if (item.issue) {
+            nameTags.push(item.issue);
+        }
+
+        if (item.firstpage || item.lastpage) {
+            let f = item.firstpage || "";
+            if (item.lastpage) {
+                f += "-" + item.lastpage;
+            }
+            nameTags.push(f);
+        }
+
+        return nameTags.join(" ");
+    }
+
+    return "";
+}
--- packages/swarm/cod-api/src/routers/structure/index.ts
+++ packages/swarm/cod-api/src/routers/structure/index.ts
@@ -1,14 +1,17 @@
 import { Db } from "mongodb";
 import Joi from "joi";
 import { Router, Request, Response } from "express";
+import { mapStructure } from '../../helpers';
 
 const structurePageValidation = Joi.number().integer().min(1).max(99999);
 const structureIdValidation = Joi.number().integer().min(1000000).max(9999999);
 const structureListValidation = Joi.array().items(structureIdValidation).min(1).max(100);
 
+
 export const getStructureRouter = ({ db }: { db: Db}) => {
     const router = Router();
 
+    // Structure details
     router.get("/:id", async (req: Request, res: Response) => {
         const { id } = req.params;
 
@@ -33,5 +36,56 @@ export const getStructureRouter = ({ db }: { db: Db}) => {
         }
     });
 
+    // Get structures list by Id's
+    router.post("/", async (req: Request, res: Response) => {
+
+        const { ids, expand } = req?.body || { ids: "[]", expand: false};
+        const structureMapper = mapStructure(expand);
+
+        let structuresIds: number[] = null;
+        try {
+            structuresIds = JSON.parse(ids);
+        } catch (e) {
+            structuresIds = [];
+        }
+
+        try {
+
+            const validationRes = structureListValidation.validate(structuresIds);
+            if (validationRes.error) {
+                return res.status(400).json({
+                    errors: [{
+                        status: 400,
+                        title: "Incorrect structure ids",
+                        detail: "Incorrect structure ids",
+                    }],
+                });
+            }
+
+
+            const data = await db.collection("structures").find({
+                _id: { $in : structuresIds },
+            })
+            .map(structureMapper)
+            .toArray();
+
+            return res.status(200).json({
+                errors: [],
+                meta: {},
+                data,
+            });
+
+        } catch (e) {
+            return res.status(500).json({
+                errors: [{
+                    status: 500,
+                    title: "Unknown error",
+                    detail: String(e),
+                }],
+            });
+        }
+
+
+    });
     return router;
 };
