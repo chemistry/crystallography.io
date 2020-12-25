701dbc42bd62abbc9f76088c8cadda44e5d7e345
Volodymyr Vreshch <vreshch@gmail.com>
2020-12-25 03:10:53 +0200

    Authors Autocomplete Endpoint

---
+++ packages/swarm/cod-api/src/routers/autocomplete/author.ts
@@ -0,0 +1,98 @@
+import Joi from "joi";
+import { Request, Response, Router } from "express";
+import { Db } from "mongodb";
+
+const RESULTS_PER_SUGGESTION = 100;
+
+export const getAuthorAutocompleteRouter = ({ db }: { db: Db }) => {
+    const router = Router();
+
+    router.get("/", async (req: Request, res: Response) => {
+        let name = String(req.query.name || "");
+        const nameChars = "\\w\\u00C0-\\u021B\\-\\`'’ιλḰṕḾŃḱóOů̅ουḿα\u2019";
+        name = name.replace(new RegExp("[^" + nameChars + "\\.\\-\\s]"), "").replace(/\s+/g, " ").trim();
+
+        const validationRes = Joi.object()
+            .keys({
+                name: Joi.string().min(1).max(255).required(),
+            })
+            .validate({
+                name,
+            });
+
+        if (validationRes.error) {
+            return res.status(400).json({
+                status: 400,
+                title: "Incorrect name for autocomplete",
+                detail: "Incorrect name for autocomplete",
+            });
+        }
+
+        try {
+            const where = buildAuthorWhere(name);
+
+            const [authorsCount, authors] = await Promise.all([
+                getAuthorsCount(where, db),
+                getAuthorsCollection(where, db),
+            ]);
+
+            return res.json({
+                meta: {
+                    limit: 100,
+                    items: authorsCount,
+                },
+                data: (authors || []),
+            });
+        } catch (e) {
+            // tslint:disable-next-line
+            console.error(e.stack);
+            return res.status(500).json({
+                errors: [String(e)],
+                meta: {},
+            });
+        }
+    });
+
+    return router;
+};
+
+function getAuthorsCount(where: any, db: Db): Promise<number> {
+    return (db.collection("authors").countDocuments(where) as any) as Promise<number>;
+}
+
+function getAuthorsCollection(where: any, db: Db) {
+    return db.collection("authors")
+        .find(where, {
+            sort: {count: -1},
+            limit: RESULTS_PER_SUGGESTION,
+        })
+        .map(({full, count}: any) => ({full, count}))
+        .toArray();
+}
+
+function buildAuthorWhere(name: string) {
+    if (name.length === 1) {
+        return {
+            a: name.toUpperCase(),
+        };
+    }
+    if (name.length === 2) {
+        return {
+            ab: name.toUpperCase(),
+        };
+    }
+    if (name.length === 3) {
+        return {
+            abc: name.toUpperCase(),
+        };
+    }
+
+    return {
+        abc: name.slice(0, 3).toUpperCase(),
+        full: new RegExp("^" + RegExpEscape(name), "i"),
+    };
+}
+
+function RegExpEscape(text: string) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+}
--- packages/swarm/cod-api/src/routers/autocomplete/index.ts
+++ packages/swarm/cod-api/src/routers/autocomplete/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { Db } from "mongodb";
+import { getAuthorAutocompleteRouter } from "./author";
 import { getNameAutocompleteRouter } from "./name";
 
 
@@ -7,6 +8,7 @@ export const getAutocompleteRouters = ({ db }: { db: Db}) => {
     const router = Router();
 
     router.use("/name", getNameAutocompleteRouter({ db }) );
+    router.use("/author", getAuthorAutocompleteRouter({ db }));
 
     return router;
 };
