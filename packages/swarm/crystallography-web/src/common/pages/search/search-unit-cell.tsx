2e4cc53ac531d8efcd662c5fd4bac02f1ff35b57
Volodymyr Vreshch <vreshch@gmail.com>
2020-12-27 04:58:28 +0200

    Unit Cell Search Form

--- packages/swarm/crystallography-web/src/common/pages/search/search-unit-cell.tsx
+++ packages/swarm/crystallography-web/src/common/pages/search/search-unit-cell.tsx
@@ -6,7 +6,62 @@ if (process.env.BROWSER) {
     require("./search-main.scss");
 }
 
-export const SearchByUnitCellPage = () => {
+const SearchByUnitCellForm = ({ onSubmit }: any) => {
+    return (
+        <div className="search-layout__search_form">
+            <div className="columns">
+                    <div className="column col-4">
+                        <label className="form-label">a</label>
+                        <input type="text" className="form-input" name="name" autoComplete="off" value=""></input>
+                    </div>
+                    <div className="column col-4">
+                        <label className="form-label">α</label>
+                        <input type="text" className="form-input" name="name" autoComplete="off" value="90"></input>
+                    </div>
+            </div>
+            <div className="columns">
+                <div className="column col-4">
+                    <label className="form-label">b</label>
+                    <input type="text" className="form-input" name="name" autoComplete="off" value=""></input>
+                </div>
+                <div className="column col-4">
+                    <label className="form-label">β</label>
+                    <input type="text" className="form-input" name="name" autoComplete="off" value="90"></input>
+                </div>
+            </div>
+            <div className="columns">
+                <div className="column col-4">
+                    <label className="form-label">c</label>
+                    <input type="text" className="form-input" name="name" autoComplete="off" value=""></input>
+                </div>
+                <div className="column col-4">
+                    <label className="form-label">γ</label>
+                    <input type="text" className="form-input" name="name" autoComplete="off" value="90"></input>
+                </div>
+            </div>
+
+            <div className="columns">
+                <div className="column col-8">
+                    <label className="form-label">tolerance (%)</label>
+                    <input type="text" className="form-input" name="name" autoComplete="off" value="1.5"></input>
+                </div>
+            </div>
+
+            <div className="columns search-layout__search_row">
+                <div className="column col-4">
+                    <button className="btn btn-active input-inline search-layout__search_btn">Search</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export const SearchByUnitCellPage = (): JSX.Element => {
+
+    const handleSubmit = (/* data: SearchFormData */) => {
+        // tslint:disable-next-line
+        console.log('-----------------');
+    }
 
     return (
         <div className="search-layout-tabs">
@@ -15,7 +70,7 @@ export const SearchByUnitCellPage = () => {
                   <SearchTab />
             </header>
             <div className="app-layout-content">
-                <h2>Search By Unit Cell</h2>
+                <SearchByUnitCellForm onSubmit={handleSubmit} />
             </div>
         </div>
     );
