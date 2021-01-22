d3c9bee8391041ebabd4693a54ab5ede7d82041a
Volodymyr Vreshch <vreshch@gmail.com>
2021-01-22 23:35:36 +0200

    Download CIF link

--- packages/containers/c14-web/src/common/components/structure-list/structure-list.tsx
+++ packages/containers/c14-web/src/common/components/structure-list/structure-list.tsx
@@ -3,6 +3,13 @@ import { NavLink } from "react-router-dom";
 import { StructureModel } from "../../models";
 import { CompoundFormula, CompoundName, AuthorsList } from "../../utils";
 
+const DownloadIcon = ()=> {
+    // tslint:disable-next-line
+    return (<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M11.6376 7.3776L8.91207 10.4088C8.68017 10.6685 8.34663 10.8168 7.99829 10.8168C7.65094 10.8168 7.3174 10.6685 7.08254 10.4078L4.35009 7.35372C4.07674 7.04722 4.10141 6.57454 4.40436 6.29889C4.70731 6.02324 5.17505 6.04712 5.44938 6.35362L7.25819 8.37571V0.746099C7.25819 0.334117 7.58976 -0.000244141 7.99829 -0.000244141C8.40781 -0.000244141 8.73839 0.334117 8.73839 0.746099V8.37969L10.5403 6.37452C10.8156 6.07001 11.2834 6.04613 11.5863 6.32377C11.8892 6.60041 11.9129 7.07111 11.6376 7.3776ZM14.5198 13.7647V9.51951C14.5198 9.10653 14.8514 8.77317 15.2599 8.77317C15.6684 8.77317 16 9.10653 16 9.51951V13.7647C16 14.9987 15.0083 15.9998 13.7836 15.9998H2.21636C0.992722 15.9998 0 14.9987 0 13.7647V9.51951C0 9.10653 0.331565 8.77317 0.740101 8.77317C1.14864 8.77317 1.4802 9.10653 1.4802 9.51951V13.7647C1.4802 14.1747 1.80979 14.5071 2.21636 14.5071H13.7836C14.1902 14.5071 14.5198 14.1747 14.5198 13.7647Z" fill="#4285F4"/>
+        </svg>
+    );
+}
+
 export const StructuresList = ({ list }: { list: StructureModel[]}) => {
     return (
         <div className="c-structure-list">{
@@ -14,6 +21,8 @@ export const StructuresList = ({ list }: { list: StructureModel[]}) => {
                                 <CompoundName model={item} />
                             </NavLink>
                         </h2>
+                        <a rel="nofollow" target="_blank" href={`/cif/${item.id}`}
+                            className="c-structure-list-item__cif_download" download><DownloadIcon /></a>
                         <p className="c-structure-list-item__formula">
                             <CompoundFormula model={item} />
                         </p>
--- packages/containers/c14-web/src/common/pages/details.tsx
+++ packages/containers/c14-web/src/common/pages/details.tsx
@@ -5,11 +5,16 @@ import { PageContainer } from "../layout";
 import { useInBrowser } from "../services";
 import { RootState } from "../store";
 import { AuthorsList, CompoundName } from "../utils";
-import { useEffect } from "react";
 
 if (process.env.BROWSER) {
     // tslint:disable-next-line
     require("./details.scss");
+}
+
+const DownloadIcon = ()=> {
+    // tslint:disable-next-line
+    return (<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M11.6375 7.37763L8.91195 10.4088C8.68005 10.6685 8.34651 10.8168 7.99817 10.8168C7.65081 10.8168 7.31728 10.6685 7.08242 10.4078L4.34996 7.35375C4.07662 7.04725 4.10129 6.57457 4.40424 6.29892C4.70719 6.02327 5.17493 6.04715 5.44926 6.35365L7.25807 8.37574V0.746129C7.25807 0.334148 7.58963 -0.000213623 7.99817 -0.000213623C8.40769 -0.000213623 8.73827 0.334148 8.73827 0.746129V8.37972L10.5402 6.37455C10.8155 6.07004 11.2832 6.04616 11.5862 6.3238C11.8891 6.60044 11.9128 7.07114 11.6375 7.37763ZM14.5197 13.7647V9.51954C14.5197 9.10656 14.8512 8.7732 15.2598 8.7732C15.6683 8.7732 15.9999 9.10656 15.9999 9.51954V13.7647C15.9999 14.9987 15.0081 15.9998 13.7835 15.9998H2.21623C0.9926 15.9998 -0.00012207 14.9987 -0.00012207 13.7647V9.51954C-0.00012207 9.10656 0.331443 8.7732 0.739979 8.7732C1.14851 8.7732 1.48008 9.10656 1.48008 9.51954V13.7647C1.48008 14.1747 1.80967 14.5071 2.21623 14.5071H13.7835C14.1901 14.5071 14.5197 14.1747 14.5197 13.7647Z" fill="white"/></svg>
+    );
 }
 
 export const DetailsPage = () => {
@@ -148,13 +153,13 @@ export const DetailsPage = () => {
                             <span dangerouslySetInnerHTML={{__html: structure.articleHtml}}></span>
                         </div> : null
                     }
-                    {
-                        structure.doi ?
-                        <div className="c-details-page__pub_item">
-                            <span className="text-bold">Link:</span>&nbsp;
-                            <a rel="nofollow" target="_blank" href={`//dx.doi.org/${structure.doi}`}>Article</a>
-                        </div> : null
-                    }
+                    <div className="c-details-page__pub_item">
+                        {
+                            structure.doi ? <a rel="nofollow" target="_blank" href={`//dx.doi.org/${structure.doi}`} className="btn btn-active input-inline c-details-page__doi_link">Article</a> : null
+                        }
+                        <a rel="nofollow" target="_blank" href={`/cif/${structure.id}`}
+                            className="btn btn-active input-inline" download><DownloadIcon />&nbsp;Download CIF</a>
+                     </div>
                 </div>
         </PageContainer>
     );
