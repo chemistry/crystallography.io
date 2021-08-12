5527e41fbba685720d5da088dde59ca92fa9e930
v <vreshch@gmail.com>
2021-08-13 00:18:45 +0200

    First GraphQL enpoint

--- packages/containers/c14-graphql-api/src/index.ts
+++ packages/containers/c14-graphql-api/src/index.ts
@@ -1,46 +1,45 @@
 import { ApolloServer, gql } from 'apollo-server';
+import { useMongo } from './common/mongo';
+import { Db } from 'mongodb';
 
 const typeDefs = gql`
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
+  type CatalogItem {
+    _id: ID
+    order: String
+    structures: [String]
   }
 
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
-    books: [Book]
+    catalogById(id:ID): CatalogItem
   }
 `;
 
-const books = [
-  {
-    title: 'The Awakening',
-    author: 'Kate Chopin',
-  },
-  {
-    title: 'City of Glass',
-    author: 'Paul Auster',
-  },
-];
-// Resolvers define the technique for fetching the types defined in the
-// schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
   Query: {
-    books: () => books,
+    catalogById: async (_parent: any, {id}: {id: string }, {db}: Context) => {
+      return await db.collection('catalog').findOne({_id: parseInt(id, 10), order: "id"});
+    }
   },
 };
+
+interface Context {
+  db: Db
+}
+
+const useContext = async ()=> {
+  const { db } = await useMongo();
+  return {
+    db
+  }
+}
 
 (async () => {
     try {
         // tslint:disable-next-line
         console.time("App Start");
 
-        const server = new ApolloServer({ typeDefs, resolvers });
+        const context = await useContext();
+        const server = new ApolloServer({ typeDefs, context, resolvers });
 
         const { url } = await server.listen();
 
