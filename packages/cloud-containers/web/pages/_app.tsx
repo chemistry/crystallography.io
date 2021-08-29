50277c14b2b67fa18dcc37d08da5ac2f3d932658
v <vreshch@gmail.com>
2021-08-29 23:55:45 +0200

    Clean up coe

--- packages/cloud-containers/web/pages/_app.tsx
+++ packages/cloud-containers/web/pages/_app.tsx
@@ -1,36 +1,19 @@
-const classNames = require("classnames");
-import React, { useEffect, useState } from "react";
 import Head from 'next/head';
 import type { AppProps } from 'next/app';
-import { CollapseIcon, LogoMobileIcon, NavBtnIcon } from "../icons";
-import { AppMobileNavigation, AppNavigation  } from "../layout";
-
-// Include Global Application Styles
-import "./app.scss";
 import '../styles/globals.css';
 
 export default function MainLayout({ Component, pageProps }: AppProps) {
-  const [isOpen, setOpen] = useState(false);
-
   return (
-    <main className={classNames({ "app": true, "is-open-navigation": isOpen })}>
-        <aside className="app-mobile-navigation">
-           <div className="app-mobile-navigation-icon" onClick={() => setOpen(!isOpen)}><NavBtnIcon /></div>
-           <div className="app-mobile-navigation-logo"><LogoMobileIcon /></div>
-        </aside>
-        <aside className="app-navigation-menu">
-          <AppMobileNavigation onClick={() => setOpen(!isOpen)} />
-        </aside>
-        <aside className="app-navigation-menu-layout" onClick={() => setOpen(false)}></aside>
-        <aside className="app-navigation">
-            <AppNavigation />
-        </aside>
-        <div className="app-collapse-button" onClick={() => setOpen(!isOpen)}>
-            <CollapseIcon />
+        <div className="app">
+            <Head>
+                <meta charSet="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta httpEquiv="X-UA-Compatible" content="IE=edge, chrome=1" />
+                <meta name="author" content="Volodymyr D. Vreshch" />
+            </Head>
+            <div>
+              <Component {...pageProps} />
+            </div>
         </div>
-        <section className="app-layout">
-          <Component {...pageProps} />
-        </section>
-    </main>
-  );
-  }
+    );
+}
