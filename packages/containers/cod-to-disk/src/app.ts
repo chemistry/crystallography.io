83a4be2886dda1cf6214aa90b2cc73cfe29da3ed
Volodymyr Vreshch <vreshch@users.noreply.github.com>
2026-03-14 04:25:43 +0100

    feat: migrate cod-to-disk from SVN to rsync (#141)
    SVN checkout stores .svn metadata that doubles disk usage (~100GB+
    for 532K structures). rsync eliminates metadata overhead, reducing
    storage to ~50GB. This allows COD data to fit on the existing 100GB
    Hetzner volume.
    
    Changes:
    - Replace svn co/update with rsync --itemize-changes
    - Update FILE_REGEX to match rsync output format
    - Remove subversion and openssh from Dockerfile (keep rsync)
    - Simplify fetchDataFromCod (rsync handles both initial and incremental sync)

--- packages/containers/cod-to-disk/src/app.ts
+++ packages/containers/cod-to-disk/src/app.ts
@@ -24,7 +24,7 @@ export interface CodFileRecord {
 }
 
 const DATA_PATH = '/home/data/cif';
-const FILE_REGEX = /^\w{1}\s+(([\w\d./]+\/(\d+)\.cif))$/i;
+const FILE_REGEX = /^>f.*\s+(([\w\d./]+\/(\d+)\.cif))$/i;
 
 let count = 0;
 const extractDataFromLogs = new Transform({
@@ -112,15 +112,10 @@ const waitTillFileExists = new Transform({
 });
 
 const fetchDataFromCod = ({ logger, execAsync }: AppContext): Readable => {
-  const isFirstStart = !fs.existsSync(DATA_PATH);
-
-  if (isFirstStart) {
-    logger.trace('First start: initial fetching data...');
-    return execAsync('svn co svn://www.crystallography.net/cod/cif ' + DATA_PATH);
-  }
-
-  logger.trace('Update SVN data...');
-  return execAsync('svn update ' + DATA_PATH);
+  logger.trace('Syncing COD data via rsync...');
+  return execAsync(
+    'rsync -avz --itemize-changes rsync://www.crystallography.net/cif/ ' + DATA_PATH + '/'
+  );
 };
 
 const synchronizeData = (context: AppContext) => {
